3f885f52653e83a055dd6902cc2278d7a5f82710
jkyu <jkyu@stanford.edu>
2024-04-12 09:50:43 -0700

    formatting for parser tests

--- test/test_meta_parser.py
+++ test/test_meta_parser.py
@@ -1,27 +1,34 @@
-from radx_reporter.studies import meta_parser
-from radx_reporter.studies import vocabulary
-from radx_reporter.studies import study
 import datetime
-import pytz
+
 import pandas as pd
 import pytest
+import pytz
+
+from radx_reporter.studies import meta_parser, study, vocabulary
+
 
 class TestMetaParser:
 
     @pytest.fixture
     def example_dataframe(self):
         data = {
-            'contribution': ['RADx-Tech', None],
-            'data_general_types.1': ['Questionnaires/Surveys; Clinical', None],
-            'description': ['There are two device studies under the 2183 banner. The 2183b study uses an enhanced visual display compared to the original study (2183a), so it has been renamed. These prospective clinical studies seek to examine the performance of the 2183 device (a and b), a lateral flow immunoassay for the point-of-care (POC) detection of SARS-CoV-2 nucleocapsid protein antigen, compared to the Roche 6800 Cobas PCR for SARS-CoV-2 assay. The goal of the 2183b study is for EUA approval for the device to be used among symptomatic persons by trained healthcare personnel collected mid-turbinate specimens. Note: no device performance data will be included in the data files.', None],
-            'estimated_participants': [488, None],
-            'institutes_supporting_study - CODED': ['NHLBI; NIBIB', None],
-            'phs': ['phs002682', "failure"],
-            'source - CODED': ['Questionnaire/Survey; Smartphone; COVID Testing Device', None],
-            'studyenddate': ['2020-11-06 16:32:30+00', None],
-            'studystartdate': ['2020-10-16 15:32:30+00', None],
-            'subject': ['Aging', None],
-            'types - CODED': ['Methods', None],
+            "contribution": ["RADx-Tech", None],
+            "data_general_types.1": ["Questionnaires/Surveys; Clinical", None],
+            "description": [
+                "There are two device studies under the 2183 banner. The 2183b study uses an enhanced visual display compared to the original study (2183a), so it has been renamed. These prospective clinical studies seek to examine the performance of the 2183 device (a and b), a lateral flow immunoassay for the point-of-care (POC) detection of SARS-CoV-2 nucleocapsid protein antigen, compared to the Roche 6800 Cobas PCR for SARS-CoV-2 assay. The goal of the 2183b study is for EUA approval for the device to be used among symptomatic persons by trained healthcare personnel collected mid-turbinate specimens. Note: no device performance data will be included in the data files.",
+                None,
+            ],
+            "estimated_participants": [488, None],
+            "institutes_supporting_study - CODED": ["NHLBI; NIBIB", None],
+            "phs": ["phs002682", "failure"],
+            "source - CODED": [
+                "Questionnaire/Survey; Smartphone; COVID Testing Device",
+                None,
+            ],
+            "studyenddate": ["2020-11-06 16:32:30+00", None],
+            "studystartdate": ["2020-10-16 15:32:30+00", None],
+            "subject": ["Aging", None],
+            "types - CODED": ["Methods", None],
         }
         return pd.DataFrame(data)
 
@@ -35,25 +42,35 @@ class TestMetaParser:
 
     def test_parse_institute(self, example_dataframe):
         institutes = meta_parser.parse_nih_institutes(example_dataframe.loc[0])
-        assert set(institutes) == {vocabulary.NihInstitute.NHLBI, vocabulary.NihInstitute.NIBIB}
+        assert set(institutes) == {
+            vocabulary.NihInstitute.NHLBI,
+            vocabulary.NihInstitute.NIBIB,
+        }
 
     def test_parse_institute_missing(self, example_dataframe):
         institutes = meta_parser.parse_nih_institutes(example_dataframe.loc[1])
         assert set(institutes) == {vocabulary.NihInstitute.MISSING}
 
     def test_parse_population(self, example_dataframe):
-        population, population_range = meta_parser.parse_population(example_dataframe.loc[0])
+        population, population_range = meta_parser.parse_population(
+            example_dataframe.loc[0]
+        )
         assert population == 488
         assert population_range == vocabulary.PopulationRange.SMALLER
 
     def test_parse_population_unknown(self, example_dataframe):
-        population, population_range = meta_parser.parse_population(example_dataframe.loc[1])
+        population, population_range = meta_parser.parse_population(
+            example_dataframe.loc[1]
+        )
         assert population is None
         assert population_range == vocabulary.PopulationRange.UNKNOWN
 
     def test_parse_data_types(self, example_dataframe):
         data_types = meta_parser.parse_data_types(example_dataframe.loc[0])
-        assert set(data_types) == {vocabulary.DataType.QUESTIONNAIRE, vocabulary.DataType.CLINICAL}
+        assert set(data_types) == {
+            vocabulary.DataType.QUESTIONNAIRE,
+            vocabulary.DataType.CLINICAL,
+        }
 
     def test_parse_data_types_missing(self, example_dataframe):
         data_types = meta_parser.parse_data_types(example_dataframe.loc[1])
@@ -69,7 +86,9 @@ class TestMetaParser:
 
     def test_parse_dates(self, example_dataframe):
         start_date, end_date = meta_parser.parse_dates(example_dataframe.loc[0])
-        assert start_date == datetime.datetime(2020, 10, 16, 15, 32, 30, tzinfo=pytz.UTC)
+        assert start_date == datetime.datetime(
+            2020, 10, 16, 15, 32, 30, tzinfo=pytz.UTC
+        )
         assert end_date == datetime.datetime(2020, 11, 6, 16, 32, 30, tzinfo=pytz.UTC)
 
     def test_parse_dates_missing(self, example_dataframe):
