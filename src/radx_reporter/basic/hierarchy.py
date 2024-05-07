919c8bede26d888fdb297dce5fb1b4a4d7cd7c5e
jkyu <jkyu@stanford.edu>
2024-05-06 17:06:32 -0700

    added preliminary hierarchy for vocabulary

---
+++ src/radx_reporter/basic/hierarchy.py
@@ -0,0 +1,182 @@
+focus_population_hierarchy = {
+    "Ethnic and Racial Groups": [
+        "African American",
+        "Native Hawaiian or other Pacific Islander",
+        "Hispanic and Latino",
+        "Asian",
+        "Immigrants",
+        "Racial and Ethnic Minorities",
+    ],
+    "Age Groups": [
+        "Children",
+        "Other Adults or Elderly",
+        "Adults",
+    ],
+    "Medically Vulnerable Populations": [
+        "People Living with HIV/AIDs",
+        "Dialysis Patients",
+        "Pregnant (or Nursing) Women",
+        "Intellectual and Developmental Disabilities",
+    ],
+    "Socioeconomic Status and Vulnerable Populations": [
+        "Lower Socioeconomic Status (SES) Population",
+        "Homeless/Unhoused",
+        "Incarcerated/Institutionalized (or Criminal Legal System Involvement)",
+        "Rural Communities",
+        "Underserved/Vulnerable Population",
+        "School Community Members",
+        "Essential Workers",
+    ],
+    "Sexual and Gender Minorities": [],
+    "UNKNOWN OR INVALID": [],
+}
+
+study_domain_hierarchy = {
+    "Testing and Surveillance": [
+        "Testing Rate/Uptake",
+        "Antigen Testing",
+        "Diagnostic Testing",
+        "Point-of-Care (POC) Testing",
+        "Screening Testing",
+        "Virological Testing",
+        "Rapid Diagnostic Testing",
+        "Laboratory (PCR) Testing",
+        "Serological (Antibody) Testing",
+        "Mobile Unit Testing",
+        "At-Home Testing",
+    ],
+    "Epidemiology and Monitoring": [
+        "COVID in School Settings",
+        "COVID Hotspots",
+        "COVID Testing Deserts",
+        "Wastewater Surveillance",
+        "Disease Surveillance",
+        "Multimodal Surveillance",
+    ],
+    "Variants and Immunity": [
+        "Variants",
+        "Immune Responses",
+        "Seroprevalence",
+    ],
+    "Pandemic Impact and Response": [
+        "Pandemic Perceptions and Decision-Making",
+        "Vaccination Rate/Uptake",
+    ],
+    "Medical Conditions": [
+        "Long COVID",
+        "Comorbidities",
+        "Multisystem Inflammatory Syndrome in Children (MIS-C)",
+        "Multisystem Inflammatory Syndrome (MIS)",
+        "Cancer",
+        "Diabetes",
+        "Obesity",
+        "Influenza",
+        "Aging",
+        "Nutrition",
+        "Mental Health",
+        "Substance Use",
+    ],
+    "Technology and Innovation": [
+        "Medical Device Development",
+        "Biosensor Technology",
+        "Novel Biosensing and VOC",
+        "Chemosensory Testing",
+        "Artifical Intelligence and Machine Learning",
+        "Next Generation Sequencing (NGS)",
+        "Digital Health Applications",
+    ],
+    "Social Factors and Public Health": [
+        "Health Behaviors",
+        "Social Determinants of Health",
+        "Community Outreach Programs",
+        "Children",
+        "Minorities",
+    ],
+    "UNKNOWN OR INVALID": [],
+}
+
+collection_method_hierarchy = {
+    "Survey": [],
+    "Interview or Focus Group": [],
+    "Wearable": [],
+    "Smartphone": [],
+    "Diagnostic Devices": {
+        "COVID-19 Testing Devices": [
+            "Antigen Testing Device",
+            "Molecular (Nucleic Acid/PCR) Testing Device",
+            "Antibody Testing / Other Adaptive Immune Response Test",
+            "Unspecified COVID Testing Device",
+        ],
+        "Breath Analysis Device / Airborne Detection Device": [],
+        "Chemosensory Testing Device": [],
+        "Electrochemical Testing Device": [],
+    },
+    "Contact Tracing": [],
+    "Wastewater Sampling": [],
+    "Disease Registry": [],
+    "Biobank Samples": [],
+    "Real-World Data": [],
+    "Other": [],
+    "UNKNOWN OR INVALID": [],
+}
+
+data_type_hierarchy = {
+    "Clinical and Medical Data": [
+        "Clinical",
+        "Electronic Medical Records",
+        "Immulogical",
+    ],
+    "Genetic and Molecular Data": [
+        "Family History",
+        "Genomic",
+        "Genotyping",
+        "Individual Genotype",
+        "Individual Phenotype",
+        "Individual Sequencing",
+        "Metabolomic",
+        "Proteomic",
+    ],
+    "Behavioral and Psychological Data": [
+        "Behavioral",
+        "Cognitive",
+        "Psychological",
+    ],
+    "Lifestyle Data": [
+        "Physical Activity",
+        "Social",
+    ],
+    "Imaging Data": [
+        "Imaging",
+    ],
+    "Enviornmental (Physical)": [],
+    "Questionnaires/Surveys": [],
+    "Supporting Documents": [],
+    "Other": [],
+    "UNKNOWN OR INVALID": [],
+}
+
+study_design_hierarchy = {
+    "Observational Studies": [
+        "Case-Control"
+        "Cross-Sectional",
+        "Longitudinal Cohort",
+        "Observational",
+        "Open Cohort",
+    ],
+    "Interventional/Clinical Trial": [],
+    "Device Validation Study": [],
+    "Genetic Studies": [
+        "Clinical Genetic Testing ",
+        "Family/Twins/Trios",
+        "Mendelian",
+        "Metagenomics",
+        "Xenograft",
+    ],
+    "Research Methodology": [
+        "Mixed Methods",
+        "Qualitative",
+        "Time-Series",
+    ],
+    "Other": [],
+    "UNKNOWN OR INVALID": [],
+}
